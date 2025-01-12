35b658210513503845f07f0772fb7d3a559165f9
Michelle Felix <mich.feloz@gmail.com>
2025-01-12 06:40:06 -0600

    feat(main): Updaiting main

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting asteroids!")
@@ -13,14 +18,49 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+    # Player
+    x = SCREEN_WIDTH // 2
+    y = SCREEN_HEIGHT // 2
+    player = Player(x, y)
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # Update updatable objects
+        for object in updatable:
+            object.update(dt)
+
+        # Check for collisions
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print('Game over!')
+                sys.exit()
+
+
         screen.fill('black')
+
+        # Drawing objects
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+        print(f'dt: {dt}')
 
 if __name__ == "__main__":
     main()
